43fc79482682db77412cb227420ee61ad5a9c927
Your Name <lmtcv1.9@gmail.com>
2024-09-02 14:56:44 +0700

    ngoc

---
+++ src/App.js
@@ -0,0 +1,30 @@
+import { useState } from 'react'
+function App() {
+    const coursts = ['JavaScript', 'PHP', 'Java']
+    
+    const [value, setValue] = useState([]);
+    console.log(value)
+    const handleChange = (e) => {
+        if(e.checked === true){
+            setValue([...value, e.className])
+            
+        }else {
+            
+        }
+        
+    }
+    
+  return (
+    <div className="App">
+      
+      <input className='JavaScript' onChange={e=>{handleChange(e.target)}} type='checkbox' />
+      <label>{coursts[0]}</label>
+      <input className='PHP' onChange={e=>{handleChange(e.target)}} type='checkbox' />
+      <label>{coursts[1]}</label>
+      <input  className='Java' onChange={e=>{handleChange(e.target)}} type='checkbox' />
+      <label>{coursts[2]}</label>
+    </div>
+  );
+}
+
+export default App;
