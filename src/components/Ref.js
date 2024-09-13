6496044de92e41ac613e725cc7a95c521f1fd033
HNgoc <144669382+hngoc-29@users.noreply.github.com>
2024-09-13 13:47:38 +0700

    Update Ref.js

--- src/components/Ref.js
+++ src/components/Ref.js
@@ -2,9 +2,13 @@ import { useState, useRef, useEffect } from 'react'
 
 //useRef dùng để lưu các giá trị bên ngoài function component
 //khi function component được re-render thì giá trị được dùng useRef không bị set lại
+
 function Ref() {
     const [count, setCount] = useState(0)
     const count2 = useRef()//giá trị mặc định sẽ ko được đặt lại khi component re-render
+    //useRef có thể truyền vào một thẻ html để lấy nó tương tự như querySelector
+    const tagH2 = useRef()
+    console.log(tagH2)
     useEffect(() => {
         setTimeout(() => {
             setCount(count + 1)
@@ -14,7 +18,7 @@ function Ref() {
 
 return (
     <>
-        <h2>Giá trị hiện tại: {count}</h2>
+        <h2 ref={tagH2}>Giá trị hiện tại: {count}</h2>
         <h2>Giá trị trước đó: {count2.current}</h2>
     </>
 )
