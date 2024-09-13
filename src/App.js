daa83ada70b3b78f2d4c2d2747cb611c465a108a
HNgoc <144669382+name1234ok@users.noreply.github.com>
2024-09-13 04:45:45 +0000

    ngoc

--- src/App.js
+++ src/App.js
@@ -3,13 +3,20 @@ import Effect from './components/Effect-1'
 import Effect2 from './components/Effect-2'
 import Todolist from './components/Todolist'
 import Todolist2 from './components/Todolist2'
+import Ref from './components/Ref'
 import { useState } from 'react'
 function App() {
   const [hien, setHien] = useState(false)
   return (
+    //khi dùng useEffect
+        /*
     <div style={{textAlign: 'center'}}>
       <button onClick={() => setHien(!hien)}>Toggle</button>
       {hien && <Effect2 />}
+    </div>
+*/
+    <div>
+      <Ref />
     </div>
   )
 }
---
+++ src/components/Ref.js
@@ -0,0 +1,24 @@
+import { useState, useRef, useEffect } from 'react'
+
+//useRef dùng để lưu các giá trị bên ngoài function component
+//khi function component được re-render thì giá trị được dùng useRef không bị set lại
+function Ref() {
+    const [count, setCount] = useState(0)
+    const count2 = useRef()//giá trị mặc định sẽ ko được đặt lại khi component re-render
+    useEffect(() => {
+        setTimeout(() => {
+            setCount(count + 1)
+            count2.current = count
+        }, 1000)
+    }, [count])
+
+return (
+    <>
+        <h2>Giá trị hiện tại: {count}</h2>
+        <h2>Giá trị trước đó: {count2.current}</h2>
+        
+    </>
+)
+}
+
+export default Ref;
